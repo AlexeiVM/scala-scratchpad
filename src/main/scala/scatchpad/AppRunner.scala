552cac7aac4bc78bec7d31ed8a15e271a1f574b9
AlexeiVM <alex.vic.makarov@gmail.com>
2016-02-09 22:41:39 -0500

    checkpoint

--- src/main/scala/scatchpad/AppRunner.scala
+++ src/main/scala/scatchpad/AppRunner.scala
@@ -2,5 +2,12 @@ package scatchpad
 
 object AppRunner extends App {
   println("Starting run ...")
+
+  val regex = "(?s)(?i)(.*)(e[.]\\s{0,3}g[.])(.*)".r
+  "ab\n\nc E. g. xyz" match {
+    case regex(a, b, c) => println(s"${b}")
+    case word @ _ => println(s"no match - $word")
+  }
+
   println("Completed run")
 }
--- src/main/scala/scatchpad/Parser.scala
+++ src/main/scala/scatchpad/Parser.scala
@@ -1,10 +1,37 @@
 package scatchpad
 
-/**
-  * Created by Alex on 2/9/2016.
-  */
-trait Parser {
-  def parseText(text: String): Seq[Sentence] = ???
+import scala.annotation.tailrec
 
-  private def findElements(text: UnparsedText, textElements: Seq[String]): Seq[TextElement] = ???
+trait Parser {
+  def parseText(text: String): Seq[Sentence] = {
+    val textElements = TextElementPatterns.patterns.foldLeft(Seq[TextElement](NotParsedText(text))) {
+      (result, pattern) =>
+        splitText(result, pattern)
+    }
+    Seq.empty
+  }
+
+  def buildRegextFromPattern(tokenPattern: String) = s"(?s)(.*)($tokenPattern)(.*)".r
+
+  @tailrec
+  private def splitText(textElements: Seq[TextElement], elementPattern: TextElementPattern): Seq[TextElement] = {
+    val regex = buildRegextFromPattern(elementPattern.pattern)
+    textElements.map {
+      case p: ParsedText => Seq(p)
+      case n: NotParsedText => {
+        n.text match {
+          case regex(headText, matchedText, tailText) => {
+            val token = elementPattern match {
+              case x: SpecialWord => Word(matchedText)
+              case x: Punctuation => Separator(matchedText)
+            }
+            val newTextElements: Seq[TextElement] = Seq(NotParsedText(headText), ParsedText(token), NotParsedText(tailText))
+            splitText(newTextElements, elementPattern)
+          }
+          case _ => Seq(n)
+        }
+      }
+
+    }.flatten
+  }
 }
