8a99a69be07b5678723cadb97d94df4a4fcdf88e
AlexeiVM <alex.vic.makarov@gmail.com>
2016-02-09 23:25:13 -0500

    checkpoint

--- src/main/scala/scatchpad/Parser.scala
+++ src/main/scala/scatchpad/Parser.scala
@@ -6,32 +6,37 @@ trait Parser {
   def parseText(text: String): Seq[Sentence] = {
     val textElements = TextElementPatterns.patterns.foldLeft(Seq[TextElement](NotParsedText(text))) {
       (result, pattern) =>
-        splitText(result, pattern)
+        result.map(x => splitText(x, pattern)).flatten
     }
     Seq.empty
   }
 
   def buildRegextFromPattern(tokenPattern: String) = s"(?s)(.*)($tokenPattern)(.*)".r
 
-  @tailrec
-  private def splitText(textElements: Seq[TextElement], elementPattern: TextElementPattern): Seq[TextElement] = {
+  def splitText(textElement: TextElement, elementPattern: TextElementPattern): Seq[TextElement] = {
     val regex = buildRegextFromPattern(elementPattern.pattern)
-    textElements.map {
+    textElement match {
       case p: ParsedText => Seq(p)
-      case n: NotParsedText => {
-        n.text match {
-          case regex(headText, matchedText, tailText) => {
-            val token = elementPattern match {
-              case x: SpecialWord => Word(matchedText)
-              case x: Punctuation => Separator(matchedText)
-            }
-            val newTextElements: Seq[TextElement] = Seq(NotParsedText(headText), ParsedText(token), NotParsedText(tailText))
-            splitText(newTextElements, elementPattern)
-          }
-          case _ => Seq(n)
-        }
-      }
+      case n: NotParsedText => inner(n.text, elementPattern, Seq.empty)
+    }
 
-    }.flatten
+    @tailrec
+    def inner(text: String, elementPattern: TextElementPattern, acc: Seq[TextElement]): Seq[TextElement] = {
+      text match {
+        case regex(headText, matchedText, tailText) => {
+          val parsedText = elementPattern match {
+            case x: SpecialWord => ParsedText(Word(matchedText))
+            case x: Punctuation => ParsedText(Separator(matchedText))
+          }
+          val addedElements =
+            if (tailText.size > 0)
+            Seq(parsedText, NotParsedText(tailText))
+          else
+            Seq(parsedText)
+          inner(headText, elementPattern, addedElements ++ acc)
+        }
+        case _ => NotParsedText(text) +: acc
+      }
+    }
   }
 }
--- src/main/scala/scatchpad/model.scala
+++ src/main/scala/scatchpad/model.scala
@@ -6,17 +6,38 @@ package scatchpad
 sealed trait Token
 
 final case class Word(letters: String) extends Token
-final case class Separator(chars: Seq[String]) extends Token
+
+final case class Separator(chars: String) extends Token
+
 final case object EndOfSentence extends Token
 
 sealed trait TextElement
-final case class UnparsedText(value: String) extends TextElement
-final case class ParsedText(tokens: Seq[Token]) extends TextElement
 
-object SpecialTextElements {
-  val spacers = Seq(" ", "\n", "\t")
-  val punctuationSigns = Seq("-", ",", ";", ":", "(", ")")
-  val specialWords = Seq("etc.", "e.g.", "i.e.", "e.\ng.", "i.\ne.", "et al", "et\nal")
+final case class ParsedText[T <: Token](tokens: T) extends TextElement
+
+final case class NotParsedText(text: String) extends TextElement
+
+sealed trait TextElementPattern {
+  val pattern: String
+}
+
+final case class SpecialWord(pattern: String) extends TextElementPattern
+
+final case class Punctuation(pattern: String) extends TextElementPattern
+
+
+object TextElementPatterns {
+  val patterns = Seq[TextElementPattern](SpecialWord("etc."),
+    SpecialWord("e[.]\\s*g[.]"),
+    SpecialWord("i[.]\\s*e[.]"),
+    SpecialWord("et\\s*al"),
+    Punctuation("\\s*-\\s*"),
+    Punctuation(","),
+    Punctuation(":"),
+    Punctuation(";"),
+    Punctuation("("),
+    Punctuation(")")
+  )
 }
 
 final case class Sentence(elements: Seq[Token])
