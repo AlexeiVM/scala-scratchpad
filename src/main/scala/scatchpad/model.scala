e3da7ecc5777f3618b51008136fd899234f9c51d
AlexeiVM <alex.vic.makarov@gmail.com>
2016-02-15 17:43:33 -0500

    checkpoint

--- src/main/scala/scatchpad/model.scala
+++ src/main/scala/scatchpad/model.scala
@@ -1,43 +1,65 @@
 package scatchpad
 
-/**
-  * Created by Alex on 2/8/2016.
-  */
+import scala.reflect._
+
 sealed trait Token
 
 final case class Word(letters: String) extends Token
 
 final case class Separator(chars: String) extends Token
 
-final case object EndOfSentence extends Token
+final case class EndOfSentence(chars: String) extends Token
 
-sealed trait TextElement
+case class TokenPattern[T <: Token : ClassTag](pattern: String) {
+  def regex = s"(?s)(?i)(.*)($pattern)".r
 
-final case class ParsedText[T <: Token](token: T) extends TextElement
-
-final case class NotParsedText(text: String) extends TextElement
-
-sealed trait TextElementPattern {
-  val pattern: String
+  def token(text: String): T = {
+    this match {
+      case _ if classTag[T] == classTag[Word] => Word(text).asInstanceOf[T]
+      case _ if classTag[T] == classTag[Separator] => Separator(text).asInstanceOf[T]
+      case _ if classTag[T] == classTag[EndOfSentence] => EndOfSentence(text).asInstanceOf[T]
+    }
+  }
 }
 
-final case class SpecialWord(pattern: String) extends TextElementPattern
-
-final case class Punctuation(pattern: String) extends TextElementPattern
-
-
-object TextElementPatterns {
-  val patterns = Seq[TextElementPattern](SpecialWord("etc."),
-    SpecialWord("e[.]\\s*g[.]"),
-    SpecialWord("i[.]\\s*e[.]"),
-    SpecialWord("et\\s*al"),
-    Punctuation("\\s*-\\s*"),
-    Punctuation(","),
-    Punctuation(":"),
-    Punctuation(";"),
-    Punctuation("("),
-    Punctuation(")")
+object TokenPatterns {
+  val items = List(
+    TokenPattern[Word]("e[.][ |\t]*(?:\\r\\n){0,1}g[.]"),
+    TokenPattern[Word]("i[.][ |\t]*(?:\\r\\n){0,1}e[.]"),
+    TokenPattern[Word]("etc."),
+    TokenPattern[Word]("et[ |\t]*(?:\\r\\n){0,1}al"),
+    TokenPattern[Word]("\\b\\w+[-](?:[ |\t]?\\r\\n)?\\w+$"),
+    TokenPattern[Separator]("\\s+-\\s+"),
+    TokenPattern[Separator]("[,|:|;|\\(|\\)]"),
+    TokenPattern[EndOfSentence]("[!]+"),
+    TokenPattern[EndOfSentence]("[.]+|[?]+|(?:[!]+)|(?:\\r\\n)+$"),
+    TokenPattern[Separator]("\\s+"),
+    TokenPattern[Word]("\\b\\w+")
   )
 }
 
-final case class Sentence(elements: Seq[Token])
+final case class Sentence(elements: Seq[Token]) {
+  def append(token: Token): Sentence = Sentence(this.elements :+ token)
+
+  def isIncomplete = {
+    val lastElement = this.elements.reverse.headOption
+    lastElement match {
+      case Some(EndOfSentence(_)) => false
+      case _ => true
+    }
+  }
+}
+
+final case class Text(sentences: Seq[Sentence]) {
+  def appendToken(token: Token) = {
+    val lastSentence = sentences.reverse.headOption.getOrElse(Sentence(Seq.empty))
+    val newTail = token match {
+      case v: EndOfSentence => Seq(lastSentence.append(v))
+      case v@_ if lastSentence.isIncomplete => Seq(lastSentence.append(v))
+      case v@_ => Seq(lastSentence, Sentence(Seq(v)))
+    }
+    val newSentences = sentences.take(sentences.size - 1) ++ newTail
+    Text(newSentences)
+  }
+
+}
